93aa898db09d4e619ea593520748e24a596cd0fd
Hajo Nils Krabbenhöft <fxtentacle@googlemail.com>
2015-04-06 23:07:27 +0700

    zip for easier download

--- scripts/build.sh
+++ scripts/build.sh
@@ -20,6 +20,8 @@ curl -L $tarball_url | tar xzv
 		 '--disable-silent-rules' 'CFLAGS=-g -O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security' 'CPPFLAGS=-D_FORTIFY_SOURCE=2' 'CXXFLAGS=-g -O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security' 'FFLAGS=-g -O2 -fstack-protector --param=ssp-buffer-size=4' 'GCJFLAGS=-g -O2 -fstack-protector --param=ssp-buffer-size=4' 'LDFLAGS=-Wl,-Bsymbolic-functions -Wl,-z,relro' '--with-modules' '--with-gs-font-dir=/usr/share/fonts/type1/gsfonts' '--with-magick-plus-plus' '--with-djvu' '--with-wmf' '--without-gvc' '--enable-shared' '--without-dps' '--without-fpx' '--x-includes=/usr/include/X11' '--x-libraries=/usr/lib/X11'
 	
 	make install
+	
+	zip -9r /tmp/imagemagick.zip /tmp/imagemagick
 )
 
 while true
