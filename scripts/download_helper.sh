70c23601d50b890b2ecb3b5da5da5da6c82909b7
Hajo Nils Krabbenhöft <fxtentacle@googlemail.com>
2018-06-22 19:01:29 +0200

    update script for binaries-cedar-14 folder

--- scripts/download_helper.sh
+++ scripts/download_helper.sh
@@ -1,4 +1,5 @@
-# before using this, set HEROKUAPPURL to something useful. e.g. HEROKUAPPURL="https://pure-savannah-1815.herokuapp.com/"
+# before using this, set HEROKUAPPURL to something useful. e.g. 
+# export HEROKUAPPURL="https://pure-savannah-1815.herokuapp.com/"
 
 mkdir /tmp/imagemagick-dl
 cd /tmp/imagemagick-dl
@@ -10,25 +11,25 @@ mv  /tmp/imagemagick-dl/tmp/imagemagick/bin/* /app/bin
 mv  /tmp/imagemagick-dl/tmp/imagemagick/lib/libMagick* /app/bin
 
 
+mkdir -p /gitosis/imagemagick-buildpack/binaries-cedar-14/*
+rm -r /gitosis/imagemagick-buildpack/binaries-cedar-14/*
+mv  /tmp/imagemagick-dl/tmp/imagemagick/bin/* /gitosis/imagemagick-buildpack/binaries-cedar-14
+mv  /tmp/imagemagick-dl/tmp/imagemagick/lib/libMagick* /gitosis/imagemagick-buildpack/binaries-cedar-14
 
-rm -r /gitosis/imagemagick-buildpack/bin/cedar-14/*
-mv  /tmp/imagemagick-dl/tmp/imagemagick/bin/* /gitosis/imagemagick-buildpack/bin/cedar-14
-mv  /tmp/imagemagick-dl/tmp/imagemagick/lib/libMagick* /gitosis/imagemagick-buildpack/bin/cedar-14
+rm binaries-cedar-14/libMagick++-6.Q16.so
+ln -s libMagick++-6.Q16.so.6.0.0 binaries-cedar-14/libMagick++-6.Q16.so
+rm binaries-cedar-14/libMagick++-6.Q16.so.6
+ln -s libMagick++-6.Q16.so.6.0.0 binaries-cedar-14/libMagick++-6.Q16.so.6
 
-rm bin/cedar-14/libMagick++-6.Q16.so
-ln -s libMagick++-6.Q16.so.6.0.0 bin/cedar-14/libMagick++-6.Q16.so
-rm bin/cedar-14/libMagick++-6.Q16.so.6
-ln -s libMagick++-6.Q16.so.6.0.0 bin/cedar-14/libMagick++-6.Q16.so.6
+rm binaries-cedar-14/libMagickCore-6.Q16.so
+ln -s libMagickCore-6.Q16.so.2.0.0 binaries-cedar-14/libMagickCore-6.Q16.so
+rm binaries-cedar-14/libMagickCore-6.Q16.so.2
+ln -s libMagickCore-6.Q16.so.2.0.0 binaries-cedar-14/libMagickCore-6.Q16.so.2
 
-rm bin/cedar-14/libMagickCore-6.Q16.so
-ln -s libMagickCore-6.Q16.so.2.0.0 bin/cedar-14/libMagickCore-6.Q16.so
-rm bin/cedar-14/libMagickCore-6.Q16.so.2
-ln -s libMagickCore-6.Q16.so.2.0.0 bin/cedar-14/libMagickCore-6.Q16.so.2
-
-rm bin/cedar-14/libMagickWand-6.Q16.so
-ln -s libMagickWand-6.Q16.so.2.0.0 bin/cedar-14/libMagickWand-6.Q16.so
-rm bin/cedar-14/libMagickWand-6.Q16.so.2
-ln -s libMagickWand-6.Q16.so.2.0.0 bin/cedar-14/libMagickWand-6.Q16.so.2
+rm binaries-cedar-14/libMagickWand-6.Q16.so
+ln -s libMagickWand-6.Q16.so.2.0.0 binaries-cedar-14/libMagickWand-6.Q16.so
+rm binaries-cedar-14/libMagickWand-6.Q16.so.2
+ln -s libMagickWand-6.Q16.so.2.0.0 binaries-cedar-14/libMagickWand-6.Q16.so.2
 
 cd -
 rm -r /tmp/imagemagick-dl
